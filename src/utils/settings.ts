392760300b43388fe97be884edb4443e34a5492b
usar0v <usarovuwww@gmail.com>
2022-01-29 08:31:02 +0600

    add-hooks

---
+++ src/utils/settings.ts
@@ -0,0 +1,4 @@
+export const DEBUG_MODE = process.env.NODE_ENV !== 'production';
+
+export const API_URL ='http://172.20.10.7:6001';
+export const BOT_TOKEN = '5102426208:AAEzf_LBLxKkDQNuJH2s0iwKMy31Bcvwr5o';
