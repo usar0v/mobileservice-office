74596d7816d5eb1972c89cd4bde5ed9d58ab108c
usar0v <usarovuwww@gmail.com>
2022-01-29 10:57:27 +0600

    fix userSlice

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,43 @@
-import React, {FC} from 'react';
+import React, {FC, useEffect, useState} from 'react';
 import AppPage from "./pages/AppPage";
 import AuthPage from "./pages/AuthPage";
 import {useAppSelector} from "./hooks";
+import requester from "./utils/requester";
+import {useDispatch} from "react-redux";
+import {setUser} from "./store/slices/userSlice";
+import Spinner from "./utils/spinner";
 
 const App: FC = () => {
+  const [loading, setLoading] = useState(false);
+
   const {isAuth} = useAppSelector(state => state.user);
+  const dispatch = useDispatch();
 
-  if (isAuth) {
-    return <AppPage/>
+  useEffect(() => {
+    authCheck();
+  }, []);
+
+  const authCheck = () => {
+    setLoading(true);
+    requester.get('auth/check')
+      .then(({token}) => {
+        dispatch(setUser({token}));
+      })
+      .catch((err) => {
+        console.log(err.response.data);
+      }).finally(() => {
+      setLoading(false);
+    })
+  };
+
+  if (loading) {
+    return <Spinner/>
   }
-
-  return <AuthPage/>
-};
+  return (
+    <>
+      {isAuth ? <AppPage/> : <AuthPage/>}
+    </>
+  );
+}
 
 export default App;
